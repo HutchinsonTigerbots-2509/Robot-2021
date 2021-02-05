43a29fdb3d29f0483c39bff89aa3557eb5350929
FerisJumbo <colegartner1@student.isd423.org>
2021-02-05 10:38:45 -0600

    Remade javadoc

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,7 +5,9 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * Constants class.
+ * 
+ * <p>The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
@@ -14,7 +16,7 @@ package frc.robot;
  * 
  * <p>When declaring constants, they should start with k. (i.e. kLeftMotorID).
  * 
- * @version 0.1.0 February 4, 2021
+ * @version 0.1.0 February 5, 2021
  * @author First Generated
  */
 public final class Constants {
