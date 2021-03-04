a85e627b587af5bdd307ddab8e58657a58b86283
CRahne <coleprahne@gmail.com>
2021-03-03 19:20:01 -0600

    Clean Up Clean Up Everybody everywhere pt 2

--- src/main/java/frc/robot/commands/ProcessAndGrabBalls.java
+++ src/main/java/frc/robot/commands/ProcessAndGrabBalls.java
@@ -2,7 +2,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.subsystems.Drivetrain.Drivetrain;
 import frc.robot.subsystems.Vision.FoundContour;
 import frc.robot.subsystems.Intake.Intake;
